7abdc38ad8e60c61bca2abd25e61ef70137e7f1b
Andrews Reis <andrewsdosreis@gmail.com>
2021-09-09 23:19:35 -0300

    Adiciona estrutura inicial do projeto

---
+++ src/main/java/br/com/lutadeclasses/gameplayservice/GameplayServiceApplication.java
@@ -0,0 +1,13 @@
+package br.com.lutadeclasses.gameplayservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameplayServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameplayServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/lutadeclasses/gameplayservice/GameplayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.lutadeclasses.gameplayservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameplayServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
